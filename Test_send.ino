d2f3f8e68f10c3447580bf39b22fd34c1131cf8e
parth-5097 <parth5097@gmail.com>
2019-04-10 19:09:56 +0530

    6

---
+++ Test_send.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0, OUTPUT);
+}
+
+void loop() {
+  analogWrite(A0,255); 
+  // put your main code here, to run repeatedly:
+
+}
